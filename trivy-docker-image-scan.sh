6b487da5d9600ddc3936d721c0506c481540add5
Anish Thomas <anishkurian.thomas@gmail.com>
2022-09-02 15:18:25 -0700

    Refactoring trivy image scan script

--- trivy-docker-image-scan.sh
+++ trivy-docker-image-scan.sh
@@ -3,8 +3,8 @@
 dockerImageName=$(awk 'NR==1 {print $2}' Dockerfile)
 echo $dockerImageName
 
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.31.3 -q --severity HIGH --exit-code 0 image $dockerImageName
-docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v $WORKSPACE:/root/.cache/ aquasec/trivy:0.31.3 -q --severity CRITICAL --exit-code 1 image $dockerImageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/trivy:/root/.cache/ aquasec/trivy:0.31.3 -q --severity HIGH --exit-code 0 image $dockerImageName
+docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/trivy:/root/.cache/ aquasec/trivy:0.31.3 -q --severity CRITICAL --exit-code 1 image $dockerImageName
 
 exit_code=$?
 echo "Exit Code: $exit_code"
